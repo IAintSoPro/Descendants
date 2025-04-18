95e8f7f23d86f7164793de39d3e337915ab0822d
Fer Lyndon Mallari <mferlyndon@yahoo.com>
2025-04-18 01:50:26 -0500

    Resolved Issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import React from 'react';
 import Home from './pages/Home.tsx'; // Import the Home page
 import About from './pages/About.tsx'; // Import the About page
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div>
+      <title>Descendants</title>
+    <p>Hello World!</p>
+    <div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+           </div>
+
+
       {/* Navigation Links */}
       <nav>
         <Link to="/">Home</Link>
