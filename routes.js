2218f35394656884b3cd0fa9a1d6a959f19d64a8
IAintSoPro <shawnmallari30@gmail.com>
2025-04-18 22:18:37 -0500

    added a default page to start on the vanilla js project

---
+++ routes.js
@@ -0,0 +1,10 @@
+  // routes.js
+  const routes = [
+    { name: "Home", path: "index.html" },
+    { name: "About", path: "about.html" },
+    { name: "Contact", path: "contact.html" }
+  ];
+  
+  // Export if using modules (optional for basic HTML setup)
+  window.ROUTES = routes;
+  
